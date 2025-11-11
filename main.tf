61c21ebbc7d0d0e0c0f97ae2f881b940cb567eec
adib-the-noob <mdadib550@gmail.com>
2025-11-11 22:38:41 +0600

    policies added

--- main.tf
+++ main.tf
@@ -13,10 +13,14 @@ provider "aws" {
 
 locals {
   users_data = yamldecode(file("./users.yml")).users
+  user_role_pair = flatten([for user in local.users_data : [for role in user.roles : {
+    username = user.username,
+    role     = role
+  }]])
 }
 
 output "users_data" {
-  value = local.users_data[*].username
+  value = local.user_role_pair
 }
 
 resource "aws_iam_user" "users" {
@@ -37,4 +41,30 @@ resource "aws_iam_user_login_profile" "profile" {
       pgp_key,
     ]
   }
+}
+
+# attaching policies
+resource "aws_iam_user_policy_attachment" "main" {
+  for_each = {
+    for pair in local.user_role_pair : "${pair.username}-${pair.role}" => pair
+  }
+  user       = aws_iam_user.users[each.value.username].name
+  policy_arn = "arn:aws:iam::aws:policy/${each.value.role}"
+}
+
+# Get AWS Account ID for console login
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value       = data.aws_caller_identity.current.account_id
+  description = "AWS Account ID for console login"
+}
+
+output "user_passwords" {
+  value = {
+    for username, profile in aws_iam_user_login_profile.profile :
+    username => profile.password
+  }
+  sensitive   = true
+  description = "Generated passwords for users (sensitive)"
 }
