897fc4afb5038e5a2c8137dbb1b62b4b4bd43b91
adib-the-noob <mdadib550@gmail.com>
2025-11-11 21:30:01 +0600

    added users

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.20.0"
     }
   }
 }
 
 provider "aws" {
-    region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 locals {
@@ -17,4 +17,24 @@ locals {
 
 output "users_data" {
   value = local.users_data[*].username
+}
+
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_data[*].username)
+  name     = each.key
+}
+
+# # password creation
+resource "aws_iam_user_login_profile" "profile" {
+  for_each        = aws_iam_user.users
+  user            = each.value.name
+  password_length = 12
+
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key,
+    ]
+  }
 }
